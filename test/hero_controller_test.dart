b7e2b596d74e78e3ffe6a4e9e4f136c67b6c95e3
Vico Melo <vinicomelo@gmail.com>
2020-10-05 16:15:48 -0400

    added sample test scenarios

---
+++ test/hero_controller_test.dart
@@ -0,0 +1,21 @@
+import 'package:heroes/model/hero.dart';
+
+import 'harness/app.dart';
+
+void main() {
+  final harness = Harness()..install();
+
+  test("POST /heroes returns 200 OK", () async {
+    await harness.agent.post("/heroes", body: {
+      "name": "Fred"
+    });
+
+    final badResponse = await harness.agent.post("/heroes", body: {
+      "name": "Fred"
+    });
+    expectResponse(badResponse, 409);
+  });
+
+
+
+}
